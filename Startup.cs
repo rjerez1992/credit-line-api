f95c55ef184ce8354e718645a0054b776e9da6cc
rjerez1992 <rjerez1992@gmail.com>
2022-03-14 17:21:29 -0300

    Base code project using NET 6

---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace CreditLineAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
